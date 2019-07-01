7fab1332bab806f08b3390195cd715f3a4351f2a
clone206 <clone206@hotmail.com>
2019-07-01 12:09:37 -0700

    Beginnings of bare-bones api for integrations.

---
+++ Roadkill.API/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Roadkill.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Roadkill Wiki API. Based on .NET Core 2.1";
+        }
+    }
+}
